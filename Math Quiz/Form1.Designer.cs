7adbe5ec0e5e97006b00863a55af7b5c462167bb
Jordon Cotton <s188044@students.aie.edu>
2019-03-28 16:11:19 -0500

    Add project files.

---
+++ Math Quiz/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Math_Quiz
+{
+    partial class MathQuiz
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1 = new System.Windows.Forms.Label();
+            this.num2 = new System.Windows.Forms.Label();
+            this.total = new System.Windows.Forms.Label();
+            this.Symbol = new System.Windows.Forms.PictureBox();
+            this.equal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.score = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Symbol)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.AutoSize = true;
+            this.num1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num1.Location = new System.Drawing.Point(37, 46);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(65, 25);
+            this.num1.TabIndex = 0;
+            this.num1.Text = "num1";
+            // 
+            // num2
+            // 
+            this.num2.AutoSize = true;
+            this.num2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num2.Location = new System.Drawing.Point(328, 46);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(65, 25);
+            this.num2.TabIndex = 1;
+            this.num2.Text = "num2";
+            // 
+            // total
+            // 
+            this.total.AutoSize = true;
+            this.total.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.total.Location = new System.Drawing.Point(568, 46);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(53, 25);
+            this.total.TabIndex = 2;
+            this.total.Text = "total";
+            // 
+            // Symbol
+            // 
+            this.Symbol.Image = global::Math_Quiz.Properties.Resources.qq;
+            this.Symbol.Location = new System.Drawing.Point(178, 29);
+            this.Symbol.Name = "Symbol";
+            this.Symbol.Size = new System.Drawing.Size(62, 42);
+            this.Symbol.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Symbol.TabIndex = 3;
+            this.Symbol.TabStop = false;
+            // 
+            // equal
+            // 
+            this.equal.AutoSize = true;
+            this.equal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.equal.Location = new System.Drawing.Point(477, 46);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(24, 25);
+            this.equal.TabIndex = 4;
+            this.equal.Text = "=";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(283, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Press P for plus, M for minus, X for multiply and D for divide";
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score.Location = new System.Drawing.Point(549, 133);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(65, 25);
+            this.score.TabIndex = 6;
+            this.score.Text = "score";
+            // 
+            // MathQuiz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 172);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.Symbol);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Name = "MathQuiz";
+            this.Text = "MathQuiz";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyisDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Symbol)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label num1;
+        private System.Windows.Forms.Label num2;
+        private System.Windows.Forms.Label total;
+        private System.Windows.Forms.PictureBox Symbol;
+        private System.Windows.Forms.Label equal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label score;
+    }
+}
+
---
+++ Math Quiz/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Math_Quiz
+{
+    public partial class MathQuiz : Form
+    {
+        int numA;
+        int numB;
+        int totalA;
+        int randMath;
+        int check;
+        int Amount = 0;
+
+        public MathQuiz()
+        {
+            InitializeComponent();
+            Start();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        { 
+        }
+
+        private void KeyisDown(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.P || e.KeyChar == 112)
+            {
+                check = 1;
+                checkStart();
+                
+            }
+
+            else if (e.KeyChar == (char)Keys.M || e.KeyChar == 109)
+            {
+                check = 2;
+                checkStart();
+               
+            }
+
+            else if (e.KeyChar == (char)Keys.X || e.KeyChar == 120)
+            {
+                check = 3;
+                checkStart();
+               
+            }
+
+            else if (e.KeyChar == (char)Keys.D || e.KeyChar == 100)
+            {
+                check = 4;
+                checkStart();
+               
+            }
+        }
+
+        public void Start()
+        {
+            
+            Random ranNumber = new Random();
+            numA = ranNumber.Next(1, 40);
+            numB = ranNumber.Next(1, 50);
+            randMath = ranNumber.Next(1, 5);
+            Symbol.Image = Properties.Resources.qq;
+
+            if (randMath < Amount)
+            {
+                Application.Exit();
+                Amount++;
+            }
+            
+            switch (randMath)
+            {
+                case 1:
+                    addNum();
+                    Console.WriteLine("Your score:" + score);
+                    break;
+                case 2:
+                    subtractNum();
+                    Console.WriteLine("Your score:" + score);
+                    break;
+                case 3:
+                    multiplyNum();
+                    Console.WriteLine("Your score:" + score);
+                    break;
+                case 4:
+                    divideNum();
+                    Console.WriteLine("Your score:" + score);
+                    break;
+                default:
+                    Console.WriteLine("Your score:" + score);
+                    break;
+            }
+            num1.Text = Convert.ToString(numA);
+            num2.Text = Convert.ToString(numB);
+            total.Text = Convert.ToString(totalA);
+        }
+
+        public void addNum()
+        {
+            totalA = numA + numB;
+        }
+        public void subtractNum()
+        {
+            totalA = numA - numB;
+        }
+        public void multiplyNum()
+        {
+            totalA = numA * numB;
+        }
+        public void divideNum()
+        {
+            totalA = numA / numB;
+        }
+
+        public void checkStart()
+        {
+            if (check == randMath)
+            {
+                Amount++;
+
+                switch (randMath)
+                {
+                    case 1:
+                        Symbol.Image = Properties.Resources.plus;
+                        MessageBox.Show("Correct");
+                        score.Text = "score:" + Amount;
+                        Start();
+                        break;
+                    case 2:
+                        Symbol.Image = Properties.Resources.minus;
+                        MessageBox.Show("Correct");
+                        score.Text = "score:" + Amount;
+                        Start();
+                        break;
+                    case 3:
+                        Symbol.Image = Properties.Resources.multi;
+                        MessageBox.Show("Correct");
+                        score.Text = "score:" + Amount;
+                        Start();
+                        break;
+                    case 4:
+                        Symbol.Image = Properties.Resources.divide;
+                        MessageBox.Show("Correct");
+                        score.Text = "score:" + Amount;
+                        Start();
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wrong Answer");
+                Symbol.Image = Properties.Resources.qq;
+                Start();
+            }
+        }
+    }
+}
+  
